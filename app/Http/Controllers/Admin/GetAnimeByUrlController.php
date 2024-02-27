ff287d1b53e5d1694f69a3aa3de6fbf7c676d31f
huynhthaihuy64 <ch4ut1nhtr1@gmail.com>
2024-02-27 16:24:31 +0700

    get list anime by image

--- app/Http/Controllers/Admin/GetAnimeByUrlController.php
+++ app/Http/Controllers/Admin/GetAnimeByUrlController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Services\UploadService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class GetAnimeByUrlController extends Controller
 {
@@ -17,5 +18,25 @@ class GetAnimeByUrlController extends Controller
     public function getAnimeByUrl(Request $request)
     {
         return $this->uploadService->getAnimeByUrl($request->only('url'));
+    }
+
+    public function getAnimeByImage(Request $request)
+    {
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+
+            $tempImagePath = $image->getPathname();
+
+            $response = Http::attach(
+                'image',
+                file_get_contents($tempImagePath),
+                $image->getClientOriginalName()
+            )->post('https://api.trace.moe/search?cutBorders');
+            unlink($tempImagePath);
+
+            return response()->json(['data' => $response->json()]);
+        } else {
+            return response()->json(['error' => 'No image provided'], 400);
+        }
     }
 }
